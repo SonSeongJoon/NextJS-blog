e66314a2b6bdfbc72e0a9af9dd79616f5d6355db
손성준 <sonsj97@gmail.com>
2023-08-01 22:11:38 +0900

    init

---
+++ src/component/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="fixed bottom-0 inset-x-0 bg-black p-1">
+            <p className="text-white text-center py-2 text-sm">{"© 2023 Company Name. All rights reserved."}</p>
+        </footer>
+    )
+}
---
+++ src/component/Header.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <header className="flex p-4 justify-between item-center">
+            <h1 className="font-bold text-3xl">SON BLOG</h1>
+            <nav className="flex items-center gap-3">
+                <Link className="hover:text-gray-200 mx-4" href="/">home</Link>
+                <Link className="hover:text-gray-200 mx-4" href="/about">about</Link>
+                <Link className="hover:text-gray-200 mx-4" href="/posts">posts</Link>
+                <Link className="hover:text-gray-200 mx-4" href="/contact">contact</Link>
+            </nav>
+        </header>
+    )
+}
