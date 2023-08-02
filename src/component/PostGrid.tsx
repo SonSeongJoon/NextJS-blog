d8f43dc031e6e558f60164235f929ea3dbe8a616
손성준 <sonsj97@gmail.com>
2023-08-02 15:25:29 +0900

    modify build error

--- src/component/PostGrid.tsx
+++ src/component/PostGrid.tsx
@@ -5,7 +5,7 @@ export default function PostGrid({posts}: Props) {
     return (
         <>
             <ul className='grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-                {posts.map((post)=><li><PostCard post={post}/></li>)}
+                {posts.map((post)=><li key={post.path}><PostCard post={post}/></li>)}
             </ul>
         </>
     )
