948c1a245c075883749299bc435c49760d51c8f5
손성준 <sonsj97@gmail.com>
2023-08-03 13:51:33 +0900

    about

--- service/post.ts
+++ service/post.ts
@@ -9,6 +9,13 @@ export type Post = {
     path: string;
     featured: boolean;
 };
+
+export type PostData = Post & {
+    content: string;
+    next: Post | null;
+    prev: Post | null;
+};
+
 export async function getNonFeaturedPosts():Promise<Post[]> {
     return getAllPosts()
         .then(posts => posts.filter((post)=> post.featured))
@@ -27,13 +34,18 @@ export async function getAllPosts(): Promise<Post[]> {
         .then((posts) => posts.sort((a,b) => (a.date > b.date ? -1:1)));
 }
 // 타입스크립트인데, 기존의 A타입과 와 B타입을 인털섹션 타입을 정의할 수 있음
-export type PostData = Post & {content: string};
 export async function getPostData(fileName: string): Promise<PostData> {
     const filePath = path.join(process.cwd(), 'data', 'posts',`${fileName}.md`);
-    const metadata = await getAllPosts().then((posts)=> posts.find((post)=> post.path === fileName));
-    if(!metadata)
+    const posts = await getAllPosts()
+    const post = posts.find((post)=> post.path === fileName);
+
+    if(!post)
         throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+
+    const index = posts.indexOf(post);
+    const next = index > 0 ? posts[index-1] : null;
+    const prev = index < posts.length - 1  ? posts[index+1]: null;
     const content = await  readFile(filePath, 'utf-8');
-    return {...metadata, content};
+    return {...post, content, next, prev};
 
 }
