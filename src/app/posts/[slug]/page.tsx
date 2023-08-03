be6238fdde8b3e1003214cef23c99acbd1a52818
손성준 <sonsj97@gmail.com>
2023-08-03 09:55:03 +0900

    ReactMarkdown(2)

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import {getPostData} from "../../../../service/post";
 import MarkdownViewer from "@/component/MarkdownViewer";
+import {AiTwotoneCalendar} from "react-icons/ai";
+import Image from "next/image";
 
 type Props = {
     params: {
@@ -9,10 +11,26 @@ type Props = {
 export default async function PostPage({params: {slug}}: Props) {
     //1.  전달된 slug에 해당하는 포스트 데이터를 읽어와서
     //2. 데이터를 마크다운뷰어에 표기하면 됨
-    const post = await getPostData(slug);
-    return <>
-        <h1>{post.title}</h1>
-        <MarkdownViewer content={post.content}/>
-    </>
+    const {title, description, date, path, content} = await getPostData(slug);
+    return <article className='rounded-2xl overflow-hidden bg-gray-100 shadow-lg'>
+        <Image
+            className='w-full h-1/5 max-h-[500px]'
+            src={`/images/posts/${path}.png`}
+            alt={title}
+            width={760}
+            height={420}
+        />
+        <section className='flex flex-col p-4'>
+            <div className='flex items-center self-end text-sky-600'>
+                <AiTwotoneCalendar/>
+                <p className='font-semibold ml-2'>{date.toString()}</p>
+            </div>
+            <h1 className='text-4xl font-bold'>{title}</h1>
+            <p className='text-xl font-bold'>{description}</p>
+            <div className='w-44 border-2 border border-sky-600 mt-4 mb-8'/>
+            <MarkdownViewer content={content}/>
+        </section>
+
+    </article>
 
 }
